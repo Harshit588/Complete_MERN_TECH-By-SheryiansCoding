a06a5bb9e2865ee66f9f2bd8832df40b2eecb6b7
Harshit Arya <123058771+Harshit588@users.noreply.github.com>
2025-04-29 22:41:13 +0530

    Lecture 1 Functions

---
+++ BackEndTechnology/lecture_1/3_Function.js
@@ -0,0 +1,22 @@
+// Normal Function
+function abcd() {
+  console.log("Hiiiii");
+}
+// abcd();
+
+// Arrow Function
+const abcd2 = () => {
+  console.log("Helllo");
+};
+// abcd2();
+
+// Using Return
+function add(a, b) {
+  return a + b;
+}
+// console.log(add(10, 20));
+
+const add2= (a, b) => {
+  return a + b;
+};
+console.log(add2(10, 20));
